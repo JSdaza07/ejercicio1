5a61b353d0618ef58f8458d91aa24f63f48ec945
Jsdaza07 <sebasdaza1706@gmail.com>
2023-08-31 00:34:55 -0500

    v2

---
+++ ejercicio.py
@@ -0,0 +1,56 @@
+class Indice:
+    def __init__(self):
+        self.__pdelta = 0
+        self.__ptheta = 0
+        self.__palfa1 = 0
+        self.__palfa2 = 0
+        self.__pbeta = 0
+        self.__pgamma = 0
+    def verPdelta(self):
+        return self.__pdelta
+    def verPtheta(self):
+        return self.__ptheta
+    def verPalfa1(self):
+        return self.__palfa1
+    def verPalfa2(self):
+        return self.__palfa2
+    def verPbeta(self):
+        return self.__pbeta
+    def verPgamma(self):
+        return self.__pgamma
+    def asignarPdelta(self,a):
+        self.__pdelta = a
+    def asignarPtheta(self,b):
+        self.__ptheta = b
+    def asignarPalfa1(self,c):
+        self.__palfa1 = c
+    def asignarPalfa2(self,d):
+        self.__palfa2 = d
+    def asignarPbeta(self,e):
+        self.__pbeta = e
+    def asignarPgamma(self,f)
+        self.__pgamma = f
+class Visita:
+    def __init__(self):
+        self.__fechavisita = ""
+        self.__registroe = ""
+        self.__notas = ""
+        self.__indices = Indice
+    def verFechavisita(self):
+        return self.__fechavisita
+    def verRegistroe(self):
+        return self.__registroe
+    def verNotas(self):
+        return self.__notas
+    def verIndices(self):
+        return self.__indices
+    def asignarFechavisita(self,g):
+        self.__fechavisita = g
+    def asignarRegistroe(self,h):
+        self.__registroe = h
+    def asignarNotas(self,i):
+        self.__notas = i
+    def asignarIndices(self,j)
+        self.__indices = j
+class Paciente:
+    
